9b8c1de7fbaa6ee73d8e356a5ca9a43d3ce6564e
Daniel Chen <chendaniely@gmail.com>
2019-12-11 00:46:55 -0500

    bash script to setup environment variables

---
+++ 01-chapter_setup.sh
@@ -0,0 +1,10 @@
+echo "Starting working directory"
+pwd
+git_book_dir="/root/git/hub/git_book_example"
+echo "Git book example directory:"
+echo $git_book_dir
+cd $git_book_dir
+echo "Current working diretory:"
+pwd
+
+git_book_src_directory="/root/git/hub/git_book"
